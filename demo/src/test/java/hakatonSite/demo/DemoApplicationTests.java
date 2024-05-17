5e14b32c30eaf1f26d02505a62acb5fc8364e7c6
BaBaBaXXX <rhbpbc321@gmail.com>
2024-05-17 20:41:23 +0500

    Upload

---
+++ demo/src/test/java/hakatonSite/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hakatonSite.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
