20e76206deb5b076e223d0128438bf9c9c1a8d30
BaBaBaXXX <rhbpbc321@gmail.com>
2024-05-17 21:06:40 +0500

    ReUpload

--- demo/src/main/java/hakatonWarpoint/site/demo/DemoApplication.java
+++ demo/src/main/java/hakatonWarpoint/site/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package hakatonSite.demo;
+package hakatonWarpoint.site.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- demo/src/test/java/hakatonWarpoint/site/demo/DemoApplicationTests.java
+++ demo/src/test/java/hakatonWarpoint/site/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package hakatonSite.demo;
+package hakatonWarpoint.site.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
